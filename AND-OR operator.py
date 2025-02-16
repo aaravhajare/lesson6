d0de266df25e0970c7955e5a89eb70245cf2789f
aaravhajare <hajareaarav66@gmail.com>
2025-02-16 09:28:08 +0530

    AND-OR operator

---
+++ AND-OR operator.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+c = 0
+
+if a and b and c :
+    print ("the condition is true")
+else :
+    print("condition is false")
+
+a = 10
+b = -10
+c = 0
+
+if a>0 or b>0 :
+    print("the either number is true")
+else :
+    print("nethier is true")
+
+if b>0 or c>0 :
+    print("either number is true")
+else :
+    print("nitheir is true")

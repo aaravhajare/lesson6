26ecf9132728ac71daed1b25cb7a18e3140c4421
aaravhajare <hajareaarav66@gmail.com>
2025-02-16 09:59:56 +0530

    homework

---
+++ homeworjk.py
@@ -0,0 +1,13 @@
+input = input("enter a character")
+
+if input >="0" and input <= "9" :
+    print("it is a number")
+
+elif input >= "a" and input <= "z":
+    print("it is an alphabet")
+
+elif input >= "A" and input <= "Z":
+    print("it is a Alphabet")
+
+else :
+    print("it is nethir alphabet nor number")

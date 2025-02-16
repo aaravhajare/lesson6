3bc88d8179be1674ead40a460e638886d45c6eae
aaravhajare <hajareaarav66@gmail.com>
2025-02-16 09:40:01 +0530

    NOT Equal Operator

---
+++ NOT Equal Operator.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "aarav"
+
+if a != b :
+    print("a is not same as b")
+
+a = 4
+b = 5
+
+if (a == 1) != (b==5) :
+    print(a,"not equal" , b)
+
+a = int(input("enter a number"))
+
+if a%2 != 0:
+    print("it is a odd number")
